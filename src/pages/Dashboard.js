792c5ccea6e5aa24919e592ddcc93d4a393c598b
unknown <jjayavardhan81@gmail.com>
2023-11-11 13:53:24 +0530

    fifth commit again

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -71,7 +71,7 @@ function Dashboard() {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
                             </svg>Add Consumers</button>
                             <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-white text-gray-800  disabled:opacity-50 disabled:pointer-events-none ">
-                            <FontAwesomeIcon icon={faUserMinus} />Manage Consumers</button>
+                            <FontAwesomeIcon icon={faUserMinus} /><Link to="/Manager">Manage Consumers</Link></button>
                     </div>
                     
                     </div>
